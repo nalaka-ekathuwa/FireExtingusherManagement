225796166494db4cbd25e223276150fbf4b51513
Nalaka Dinesh <nalakadinesh123@gmail.com>
2025-04-19 01:26:34 +0300

    fixed header bug

--- header.php
+++ header.php
@@ -125,12 +125,12 @@ if (empty($_SESSION["user_id"])) {
                                 <img src="<?php echo $session_uimg; ?>" alt="">
                             </div>
                             <div class="m-l-10">
-                                <p class="m-b-0 text-dark font-weight-semibold"><?php echo $session_uimg; ?></p>
+                                <p class="m-b-0 text-dark font-weight-semibold"><?php echo $session_uname; ?></p>
                                 <p class="m-b-0 opacity-07">Active</p>
                             </div>
                         </div>
                     </div>
-                    <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
+                    <a href="manage_user.php?key=<?php echo $sesssion_uid; ?>;" class="dropdown-item d-block p-h-15 p-v-10">
                         <div class="d-flex align-items-center justify-content-between">
                             <div>
                                 <i class="anticon opacity-04 font-size-16 anticon-user"></i>
@@ -139,15 +139,7 @@ if (empty($_SESSION["user_id"])) {
                             <i class="anticon font-size-10 anticon-right"></i>
                         </div>
                     </a>
-                    <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div>
-                                <i class="anticon opacity-04 font-size-16 anticon-lock"></i>
-                                <span class="m-l-10">Account Setting</span>
-                            </div>
-                            <i class="anticon font-size-10 anticon-right"></i>
-                        </div>
-                    </a>
+                    
                     <!-- <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
                         <div class="d-flex align-items-center justify-content-between">
                             <div>
