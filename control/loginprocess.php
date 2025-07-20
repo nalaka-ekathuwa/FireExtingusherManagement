103af81cb0066889c6c38c916d8ab800861475bf
Nalaka Dinesh <nalakadinesh123@gmail.com>
2025-07-20 04:47:36 +0300

    add super

--- control/loginprocess.php
+++ control/loginprocess.php
@@ -6,15 +6,15 @@ require_once "../config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-	$email = $conn->real_escape_string($_POST['username']);
-	$password =$conn->real_escape_string($_POST['password']);
+    $email = $conn->real_escape_string($_POST['username']);
+    $password = $conn->real_escape_string($_POST['password']);
 
-	$sql = "SELECT * FROM users WHERE email = ?";
+    $sql = "SELECT * FROM users WHERE email = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('s', $email);
     $stmt->execute();
     $result = $stmt->get_result();
-   
+
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     //  var_dump($hashed_password);exit;
 
@@ -25,12 +25,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['user_name'] = $row['name'];
             $_SESSION['user_img'] = $row['img'];
             $_SESSION['role_id'] = $row['role_id'];
-			// var_dump($row['role_id']);exit;
-            header("location: ../". ($row['role_id']==1? "customers" : "locations").".php?");
+            $_SESSION['idfirma'] = $row['idfirma'];
+            // var_dump($row['role_id']);exit;
+            $page = '';
+            switch ($row['role_id']) {
+                case '1':
+                    //Company admin
+                    $page = 'customers.php';
+                    break;
+                case '3':
+                    //Fire safety Officer
+                    $page = 'view_locations.php';
+                    break;
+                case '6':
+                    //Driver
+                    $page = 'customers.php';
+                    break;
+                case '7':
+                    //Maintain tech
+                    $page = 'customers.php';
+                    break;
+                case '9':
+                    //Super admin
+                    $page = 'company.php';
+                    break;
+                case '10':
+                    //Employee
+                    $page = 'customers.php';
+                    break;
+                case '11':
+                    //Observer
+                    $page = 'view_locations.php';
+                    break;
+                default:
+                //code block
+            }
+            header("location: ../" . $page);
         } else {
-			header("location: ../index.php?s=1");
-		}
-	} 
-	else { header("location: ../index.php?s=2");
-	}
+            header("location: ../index.php?s=1");
+        }
+    } else {
+        header("location: ../index.php?s=2");
+    }
 }
